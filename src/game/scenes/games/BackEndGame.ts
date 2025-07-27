e4ef995a70bf1eca8dea00a3940afb05d35189f7
Jzcse <jzc3294582930@gmail.com>
2025-07-27 15:59:06 +0800

    feat(BackEndGame.ts) 后端游戏怪物生成 | 后端游戏怪物自动寻敌 | 游戏失败逻辑

--- src/game/scenes/games/BackEndGame.ts
+++ src/game/scenes/games/BackEndGame.ts
@@ -1,6 +1,5 @@
 import { Scene } from "phaser";
 import {CustomerOrder} from "../Game.ts";
-import {CommonFunction} from "../../../utils/CommonFunction.ts";
 import Dungeon, {Room} from "@mikewesthad/dungeon";
 import Tileset = Phaser.Tilemaps.Tileset;
 import TilemapLayer = Phaser.Tilemaps.TilemapLayer;
@@ -15,6 +14,7 @@ export class BackEndGame extends Scene
     
     // public variables
     private player: Player;
+    private enemies: Enemy[] = [];
     private groundLayer: TilemapLayer
     private stuffLayer: TilemapLayer
     private shadowLayer: TilemapLayer
@@ -32,7 +32,7 @@ export class BackEndGame extends Scene
                         y: 0,
                         x: 0
                     },
-                    debug: false
+                    debug: true
                 }
             }
         });
@@ -57,6 +57,18 @@ export class BackEndGame extends Scene
                 spacing: 2
             }
         );
+        this.load.spritesheet("game-back-end-enemy-static", "assets/games/back-end/slime-static.png", {
+            frameWidth: 64,
+            frameHeight: 64,
+            margin: 0,
+            spacing: 0
+        });
+        this.load.spritesheet('game-back-end-enemy-walk', 'assets/games/back-end/slime-walk.png', {
+            frameWidth: 64,
+            frameHeight: 64,
+            margin: 0,
+            spacing: 0
+        })
     }
 
     create()
@@ -70,21 +82,57 @@ export class BackEndGame extends Scene
         this.createIntro();
         this.createOperation();
         
+        this.events.on("back-end-game-over", this.gameOver, this);
+        this.events.once(Phaser.Scenes.Events.DESTROY, () => {
+            this.events.off("back-end-game-over", this.gameOver, this);
+            this.enemies.forEach(enemy => {
+                enemy.destroy();
+            })
+            this.enemies = [];
+        })
     }
 
     update(time, delta) {
-        this.player.update();
+        
+        if(this.player.sprite.body) {
+            this.player.update();
 
-        const playerTileX = this.groundLayer.worldToTileX(this.player.sprite.x);
-        const playerTileY = this.groundLayer.worldToTileY(this.player.sprite.y);
-        const playerRoom = this.dungeon.getRoomAt(playerTileX, playerTileY);
+            const playerTileX = this.groundLayer.worldToTileX(this.player.sprite.x);
+            const playerTileY = this.groundLayer.worldToTileY(this.player.sprite.y);
+            const playerRoom = this.dungeon.getRoomAt(playerTileX, playerTileY);
 
-        this.tilemapVisibility.setActiveRoom(playerRoom);
+            this.tilemapVisibility.setActiveRoom(playerRoom);   
+        }
+        
+        this.enemies.forEach(enemy => {
+            enemy.update(this.player.sprite);
+        })
+    }
+    
+    gameOver(): void {
+        const centerX = this.cameras.main.width / 2 + this.cameras.main.worldView.x;
+        const centerY = this.cameras.main.height / 2 + this.cameras.main.worldView.y;
+        const gameOverText = this.add.text(centerX, centerY, 'GAME OVER', {
+            fontSize: '64px', color: '#FF6347', stroke: '#FFFFFF', strokeThickness: 8,
+            fontFamily: '"Arial Black", Gadget, sans-serif'
+        }).setOrigin(0.5);
+        gameOverText.setShadow(5, 5, 'rgba(0,0,0,0.5)', 15);
+
+        // 创建重新开始按钮
+        const restartButton = this.add.text(centerX, centerY + 100, 'Restart', {
+            fontSize: '32px', color: '#ffffff', backgroundColor: '#000000', padding: { x: 20, y: 10 }
+        }).setOrigin(0.5).setInteractive();
+
+        restartButton.on('pointerdown', () => {
+            // 重新开始游戏
+            this.scene.restart();
+        });
     }
     
     initGame() {
         const MaxRoomNumberArr: number[] = [1, 3, 5, 7, 9];
         const MaxRoomNumber: number = MaxRoomNumberArr[this.DIFFICULTY];
+        this.createAnims();
 
         this.dungeon = new Dungeon({
             width: 100,
@@ -194,6 +242,7 @@ export class BackEndGame extends Scene
         const startRoom = rooms.shift();
         const endRoom = Phaser.Utils.Array.RemoveRandomElement(rooms);
         const otherRooms = Phaser.Utils.Array.Shuffle(rooms).slice(0, rooms.length * 0.9);
+        const enemyPosition: {x: number, y: number}[] = [];
         
         this.stuffLayer.putTileAt(TILE_MAPPING.STAIRS, endRoom.centerX, endRoom.centerY);
 
@@ -219,6 +268,14 @@ export class BackEndGame extends Scene
                     this.stuffLayer.putTilesAt(TILE_MAPPING.TOWER, room.centerX + 1, room.centerY - 1);
                 }
             }
+
+            const randX: number = Phaser.Math.Between(room.left + 2, room.right - 2);
+            const randY: number = Phaser.Math.Between(room.top + 2, room.bottom - 2);
+            const worldX: number = this.stuffLayer.tileToWorldX(randX);
+            const worldY: number = this.stuffLayer.tileToWorldY(randY);
+            if (true) {
+                enemyPosition.push({x: worldX, y: worldY});
+            }
         });
 
         this.stuffLayer.setTileIndexCallback(TILE_MAPPING.STAIRS, () => {
@@ -241,10 +298,72 @@ export class BackEndGame extends Scene
         this.player = new Player(this, map.widthInPixels / 2, map.heightInPixels / 2, this.DIFFICULTY);
         this.physics.add.collider(this.player.sprite, this.groundLayer);
         this.physics.add.collider(this.player.sprite, this.stuffLayer);
+        
+        enemyPosition.forEach((pos) => {
+            const enemy = new Enemy(this, pos.x, pos.y, this.DIFFICULTY, [this.groundLayer, this.stuffLayer]);
+            this.enemies.push(enemy);
+            this.physics.add.collider(enemy.sprite, this.groundLayer);
+            this.physics.add.collider(enemy.sprite, this.stuffLayer);
+            this.physics.add.collider(this.player.sprite, enemy.sprite, this.handlePlayerEnemyCollider, undefined, this);
+        })
 
         const camera = this.cameras.main;
         camera.startFollow(this.player.sprite);
         camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    }
+    
+    handlePlayerEnemyCollider(player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody, enemy: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody) {
+        this.player.healthDecrease();
+    }
+    
+    createAnims(): void {
+        const anims = this.anims;
+        const moveDuration = 48 / Enemy.prototype.speed * 1000;
+        
+        if (!anims.exists("enemy-standby")){
+            anims.create({
+                key: "enemy-standby",
+                frames: anims.generateFrameNumbers("game-back-end-enemy-static", { start: 0, end: 5 }),
+                repeat: -1,
+                duration: 1000,
+            })
+        }
+        if (!anims.exists("enemy-walk-left")){
+            anims.create({
+                key: "enemy-walk-left",
+                frames: anims.generateFrameNumbers("game-back-end-enemy-walk", { start: 16, end: 23 }),
+                frameRate: 8,
+                repeat: 1,
+                duration: moveDuration
+            })
+        }
+        if (!anims.exists("enemy-walk-right")){
+            anims.create({
+                key: "enemy-walk-right",
+                frames: anims.generateFrameNumbers("game-back-end-enemy-walk", { start: 24, end: 31 }),
+                frameRate: 8,
+                repeat: 1,
+                duration: moveDuration
+            })
+        }
+        if (!anims.exists("enemy-walk-up")){
+            anims.create({
+                key: "enemy-walk-up",
+                frames: anims.generateFrameNumbers("game-back-end-enemy-walk", { start: 8, end: 15 }),
+                frameRate: 8,
+                repeat: 1,
+                duration: moveDuration
+            })
+        }
+        if (!anims.exists("enemy-walk-down")){
+            anims.create({
+                key: "enemy-walk-down",
+                frames: anims.generateFrameNumbers("game-back-end-enemy-walk", { start: 0, end: 7 }),
+                frameRate: 8,
+                repeat: 1,
+                duration: moveDuration
+            })
+        }
     }
     
     createTimer() {
@@ -281,18 +400,22 @@ class Player {
         this.scene = scene;
         this.DIFFICULTY = difficulty;
         const anims = this.scene.anims;
-        anims.create({
-            key: "player-walk",
-            frames: anims.generateFrameNumbers("game-back-end-player", { start: 46, end: 49 }),
-            frameRate: 8,
-            repeat: -1
-        });
-        anims.create({
-            key: "player-walk-back",
-            frames: anims.generateFrameNumbers("game-back-end-player", { start: 65, end: 68 }),
-            frameRate: 8,
-            repeat: -1
-        });
+        if (!anims.exists("player-walk")){
+            anims.create({
+                key: "player-walk",
+                frames: anims.generateFrameNumbers("game-back-end-player", { start: 46, end: 49 }),
+                frameRate: 8,
+                repeat: -1,
+            });
+        }
+        if (!anims.exists("player-walk-back")){
+            anims.create({
+                key: "player-walk-back",
+                frames: anims.generateFrameNumbers("game-back-end-player", { start: 65, end: 68 }),
+                frameRate: 8,
+                repeat: -1
+            });
+        }
 
         this.sprite = scene.physics.add
             .sprite(x, y, "game-back-end-player", 0)
@@ -357,6 +480,105 @@ class Player {
         this.sprite.destroy();
     }
     
+    healthDecrease() {
+        this.health--;
+        if (this.health <= 0) {
+            this.destroy();
+            this.scene.events.emit("back-end-game-over");
+        }
+    }
+    
+}
+
+class Enemy {
+    private scene: Scene;
+    readonly sprite;
+    private isMoving = false;
+    private tileSize = 48;
+    private readonly tileLayers: TilemapLayer[];
+    private DIFFICULTY: number;
+    public readonly speed: number = 100;
+    private health: number;
+    private sight_distance: number = 200;
+    
+    constructor(scene: Scene, x: number, y: number, difficulty: number, layers: TilemapLayer[]) {
+        this.scene = scene;
+        this.DIFFICULTY = difficulty;
+        this.tileLayers = layers;
+        
+        this.sprite = scene.physics.add
+            .sprite(x, y, 'game-back-end-enemy-static', 0)
+            .setSize(22, 23)
+            .setOffset(23, 27)
+            .setScale(1.5);
+        
+        this.sprite.anims.play("enemy-standby");
+    }
+    
+    destroy() {
+        this.sprite.destroy();
+    }
+    
+    update(player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody) {
+        const enemy = this.sprite;
+        if (!enemy.body) return;
+        
+        if(this.isMoving) return;
+        
+        const dx = player.x - enemy.x;
+        const dy = player.y - enemy.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        
+        if (distance <= this.sight_distance) {
+            this.isMoving = true;
+            let targetX = enemy.x;
+            let targetY = enemy.y;
+            
+            if (Math.abs(dx) > Math.abs(dy)) {
+                targetX += dx > 0 ? this.tileSize : -this.tileSize;
+            } else {
+                targetY += dy > 0 ? this.tileSize : -this.tileSize;
+            }
+
+            for (let i = 0; i < this.tileLayers.length; i++) {
+                const tile = this.tileLayers[i];
+                const targetTileX = tile.worldToTileX(targetX);
+                const targetTileY = tile.worldToTileY(targetY);
+                const targetTile = tile.getTileAt(targetTileX, targetTileY);
+                if (targetTile && targetTile.collides) {
+                    this.isMoving = false;
+                    enemy.body.setVelocity(0);
+                    enemy.anims.play("enemy-standby");
+                    return;
+                }
+            }
+            
+            const moveDuration = (this.tileSize / this.speed) * 1000;
+            const vx = (targetX - enemy.x) / moveDuration * 1000;
+            const vy = (targetY - enemy.y) / moveDuration * 1000;
+            
+            enemy.body.setVelocity(vx, vy);
+
+            if (vx > 0) {
+                enemy.anims.play("enemy-walk-right", true);
+            } else if (vx < 0) {
+                enemy.anims.play("enemy-walk-left", true);
+            } else if (vy > 0) {
+                enemy.anims.play("enemy-walk-down", true);
+            } else if (vy < 0) {
+                enemy.anims.play("enemy-walk-up", true);
+            }
+
+            this.scene.time.delayedCall(moveDuration, () => {
+                enemy.body.setVelocity(0);
+                this.isMoving = false;
+            });
+            
+        } else {
+            enemy.body.setVelocity(0);
+            enemy.anims.play("enemy-standby", true);
+        }
+    }
 }
 
 class TilemapVisibility {
